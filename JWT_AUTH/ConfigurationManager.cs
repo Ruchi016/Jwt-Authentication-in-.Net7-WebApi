7edf4e4ea0001216de488706ccdc63e2e874b56a
Ruchika <Ruchika@5CD23912LA-RD>
2023-07-06 15:29:34 +0530

    Add project files.

---
+++ JWT_AUTH/ConfigurationManager.cs
@@ -0,0 +1,14 @@
+﻿namespace JWT_AUTH
+{
+    static class ConfigurationManager
+    {
+        public static IConfiguration AppSetting
+        {
+            get;
+        }
+        static ConfigurationManager()
+        {
+            AppSetting = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        }
+    }
+}
